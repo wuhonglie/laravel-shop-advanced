9eac5102ebc1b6f26aa41b44c783ff0d70ccd94f
whl <2429986613@qq.com>
2020-05-13 08:52:09 +0000

    顶部导航加入商品类目树

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Http\ViewComposers\CategoryTreeComposer;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 use Monolog\Logger;
 use Yansongda\Pay\Pay;
@@ -51,6 +53,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        View::composer(['products.index', 'products.show'],
+        CategoryTreeComposer::class);
     }
 }
