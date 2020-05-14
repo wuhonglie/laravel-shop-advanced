b109ec33f50fb562067a911652f22ba1a4224945
whl <2429986613@qq.com>
2020-05-14 00:49:33 +0000

    初始化众筹模型

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,23 +5,27 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
-class Product extends Model
-{
+class Product extends Model {
+    const TYPE_NORMAL = 'normal';
+    const TYPE_CROWDFUNDING = 'crowdfunding';
+    public static $typeMap = [
+        self::TYPE_NORMAL => '普通商品',
+        self::TYPE_CROWDFUNDING => '众筹商品',
+    ];
     protected $fillable = [
         'title', 'description', 'image', 'on_sale',
-        'rating', 'sold_count', 'review_count', 'price'
+        'rating', 'sold_count', 'review_count', 'price', 'type',
     ];
     protected $casts = [
         'on_sale' => 'boolean', // on_sale 是一个布尔类型的字段
     ];
+
     // 与商品SKU关联
-    public function skus()
-    {
+    public function skus() {
         return $this->hasMany(ProductSku::class);
     }
 
-    public function getImageUrlAttribute()
-    {
+    public function getImageUrlAttribute() {
         // 如果 image 字段本身就已经是完整的 url 就直接返回
         if (Str::startsWith($this->attributes['image'], ['http://', 'https://'])) {
             return $this->attributes['image'];
@@ -29,8 +33,12 @@ class Product extends Model
         return \Storage::disk('public')->url($this->attributes['image']);
     }
 
-    public function category()
-    {
+    public function category() {
         return $this->belongsTo(Category::class);
+    }
+
+    public function crowdfunding()
+    {
+        return $this->hasOne(CrowdfundingProduct::class);
     }
 }
