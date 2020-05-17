98f29b46b9c0b42fad7aace1805578306eeadf6d
whl <2429986613@qq.com>
2020-05-17 02:00:14 +0000

    Elasticsearch 重构商品搜索功能

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -8,6 +8,7 @@ use App\Models\OrderItem;
 use App\Models\Product;
 use App\Services\CategoryService;
 use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\Auth;
 
 class ProductsController extends Controller
@@ -15,53 +16,78 @@ class ProductsController extends Controller
     public function index(Request $request)
     {
 
-        // 创建一个查询构造器
-        $builder = Product::query()->where('on_sale', true);
-        // 判断是否有提交 search 参数，如果有就赋值给 $search 变量
-        // search 参数用来模糊搜索商品
-        if ($search = $request->input('search', '')) {
-            $like = '%'.$search.'%';
-            // 模糊搜索商品标题、商品详情、SKU 标题、SKU描述
-            $builder->where(function ($query) use ($like) {
-                $query->where('title', 'like', $like)
-                    ->orWhere('description', 'like', $like)
-                    ->orWhereHas('skus', function ($query) use ($like) {
-                        $query->where('title', 'like', $like)
-                            ->orWhere('description', 'like', $like);
-                    });
-            });
-        }
-        // 如果有传入category_id 字段, 并且在数据库中有对应的类目
-        if($request->input('category_id') && $category = Category::find($request->input('category_id'))){
-            if($category->is_directory){
-                $builder->whereHas('category',function($query) use($category){
-                   $query->where('path', 'like', $category->path.$category->id.'-%');
-                });
-            }else{
-                $builder->where('category_id', $category->id);
-            }
-        }
-
-        // 是否有提交 order 参数，如果有就赋值给 $order 变量
-        // order 参数用来控制商品的排序规则
+        $page = $request->input('page', 1);
+        $perPage = 16;
+        $params = [
+            'index' => 'products',
+            'body' => [
+                'from' => ($page - 1) * $perPage,
+                'size' => $perPage,
+                'query' => [
+                    'bool' => [
+                        'filter' => [
+                            ['term' => ['on_sale' => true]],
+                        ],
+                    ],
+                ],
+            ],
+        ];
         if ($order = $request->input('order', '')) {
-            // 是否是以 _asc 或者 _desc 结尾
             if (preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
-                // 如果字符串的开头是这 3 个字符串之一，说明是一个合法的排序值
                 if (in_array($m[1], ['price', 'sold_count', 'rating'])) {
-                    // 根据传入的排序值来构造排序参数
-                    $builder->orderBy($m[1], $m[2]);
+                    $params['body']['sort'] = [[$m[1] => $m[2]]];
                 }
             }
         }
+        if ($request->input('category_id') && $category = Category::find($request->input('category_id'))) {
+            if ($category->is_directory) {
+                $params['body']['query']['bool']['filter'][] = [
+                    'prefix' => ['category_path' => $category->path . $category->id . '-'],
+                ];
+            } else {
+                $params['body']['query']['bool']['filter'][] = [
+                    'term' => ['category_id' => $category->id]
+                ];
+            }
+        }
+        if ($search = $request->input('search', '')){
+            $keywords = array_filter(explode(' ', $search));
+            $params['body']['query']['bool']['must'] = [];
+            foreach($keywords as $keyword){
+                $params['body']['query']['bool']['must'] = [
+                    [
+                        'multi_match' => [
+                            'query' => $keyword,
+                            'fields' => [
+                                'title^3',
+                                'long_title^2',
+                                'category^2',
+                                'description',
+                                'skus_title',
+                                'skus_description',
+                                'properties_value',
+                            ]
+                        ]
+                    ]
+                ];
+            }
 
-        $products = $builder->paginate(16);
+        }
+        $result = app('es')->search($params);
+        $productIds = collect($result['hits']['hits'])->pluck('_id')->all();
+        $products = Product::query()
+            ->whereIn('id', $productIds)
+            ->orderByRaw(sprintf("FIND_IN_SET(id,'%s')", join(',', $productIds)))
+            ->get();
+        $pager = new LengthAwarePaginator($products, $result['hits']['total']['value'], $perPage, $page, [
+            'path' => route('products.index'),
+        ]);
 
         return view('products.index', [
-            'products' => $products,
-            'filters'  => [
+            'products' => $pager,
+            'filters' => [
                 'search' => $search,
-                'order'  => $order,
+                'order' => $order,
             ],
             'category' => $category ?? null,
         ]);
@@ -76,7 +102,7 @@ class ProductsController extends Controller
 
         $favored = false;
         // 用户未登录时返回的是 null，已登录时返回的是对应的用户对象
-        if($user = $request->user()) {
+        if ($user = $request->user()) {
             // 从当前用户已收藏的商品中搜索 id 为当前商品 id 的商品
             // boolval() 函数用于把值转为布尔值
             $favored = boolval($user->favoriteProducts()->find($product->id));
