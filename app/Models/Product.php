0945fbea08ffa2914f9b725797fad86c6dd0ebbf
whl <2429986613@qq.com>
2020-05-25 01:00:26 +0000

    秒杀商品管理后台

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,9 +10,11 @@ class Product extends Model
 {
     const TYPE_NORMAL = 'normal';
     const TYPE_CROWDFUNDING = 'crowdfunding';
+    const TYPE_SECKILL = 'seckill';
     public static $typeMap = [
         self::TYPE_NORMAL => '普通商品',
         self::TYPE_CROWDFUNDING => '众筹商品',
+        self::TYPE_SECKILL => '秒杀商品',
     ];
     protected $fillable = [
         'title', 'description', 'image', 'on_sale',
