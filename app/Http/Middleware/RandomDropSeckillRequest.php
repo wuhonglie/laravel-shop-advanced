390f91e4ba32a6f98607b23dad01388589b1adec
whl <2429986613@qq.com>
2020-05-27 01:29:46 +0000

    添加log

--- app/Http/Middleware/RandomDropSeckillRequest.php
+++ app/Http/Middleware/RandomDropSeckillRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use App\Exceptions\InvalidRequestException;
 use Closure;
+use Illuminate\Support\Facades\Log;
 
 class RandomDropSeckillRequest
 {
@@ -16,7 +17,9 @@ class RandomDropSeckillRequest
      */
     public function handle($request, Closure $next, $percent)
     {
+        Log::info('fff');
         if (random_int(0, 100) < (int)$percent) {
+            Log::info('ddd');
             throw new InvalidRequestException('参与的用户过多，请稍后再试', 403);
         }
         return $next($request);
